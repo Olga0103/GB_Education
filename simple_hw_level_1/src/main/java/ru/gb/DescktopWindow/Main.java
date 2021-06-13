070c78786dee78d89cd6feae0a3c7bdb93bf0a69
Olga0103 <nezabudka-84@mail.ru>
2021-06-13 18:13:15 +0300

    simple_hw_level_1

---
+++ simple_hw_level_1/src/main/java/ru/gb/DescktopWindow/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.DescktopWindow;
+
+public class Main {
+
+    public static void main(String[] args){
+        Window window = new Window();
+    }
+
+}
---
+++ simple_hw_level_1/src/main/java/ru/gb/DescktopWindow/Window.java
@@ -0,0 +1,50 @@
+package ru.gb.DescktopWindow;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame{
+
+    public Window(){
+
+        super.setVisible(true);
+
+        setTitle("Test Window");
+        setLocation(650, 250);
+        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        GridLayout layout = new GridLayout(1, 2);
+        JPanel panel = new JPanel(layout);
+
+        JButton btn1 = new JButton("EXIT");
+        add(btn1, BorderLayout.SOUTH);
+        btn1.setBackground(Color.blue);
+        btn1.setForeground(Color.WHITE);
+
+        Exit exit = new Exit();
+        btn1.addActionListener(exit);
+
+        JButton btn3 = new JButton("NULL");
+        panel.add(btn3);
+        add(panel, BorderLayout.NORTH);
+
+        JTextField field = new JTextField();
+        Font font = new Font("TimesRoman", Font.BOLD, 35);
+        field.setBackground(Color.lightGray);
+        add(field, BorderLayout.CENTER);
+        field.setForeground(Color.WHITE);
+        field.setFont(font);
+        field.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Введенный текст: " + field.getText());
+            }
+        });
+        setVisible(true);
+    }
+
+}
