68be432be283b77c4881f83481205b629bea80b6
Olga0103 <nezabudka-84@mail.ru>
2021-06-13 18:03:54 +0300

    simple_hw_level_1

---
+++ simple_hw_level_1/src/main/java/ru/gb/hungry_cats/Plate.java
@@ -0,0 +1,23 @@
+package ru.gb.hungry_cats;
+
+public class Plate{
+
+    public int food;
+
+    public Plate(int food){
+        this.food = food;
+    }
+
+    public void decreaseFood(int val){
+        this.food -= val;
+    }
+
+    public void increaseFood(int n){
+        food += n;
+    }
+
+    public void info(){
+        System.out.println("В тарелке " + food + " единиц корма");
+    }
+
+}
