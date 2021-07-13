a5a5752752a5e9cc106e3103e48fb7c541f8f19d
Olga0103 <nezabudka-84@mail.ru>
2021-07-13 14:34:31 +0300

    simple_hw

---
+++ simple_hw/src/main/java/ru/gb/examples_of_patterns/identity_map/java/Main.java
@@ -0,0 +1,13 @@
+package ru.gb.examples_of_patterns.identity_map.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        UserMapper.createTable();
+        UserMapper.addUser(new User(1,"2",3));
+        UserMapper.findById(1L);
+        UserMapper.findById(1L);
+        UserMapper.deleteById(121L);
+    }
+}
